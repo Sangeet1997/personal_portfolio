f93086d064a2ff960f7486f5eb932ef0288c7b94
Sangeet1997 <sangeetsaha1116@gmail.com>
2025-02-13 22:14:30 -0500

    chages to vit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path, { dirname } from "path";
+import path, { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 
 import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
-// import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 // https://vite.dev/config/
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 export default defineConfig({
   plugins: [react(), themePlugin()],
+  base: "/personal_portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
