d2c10e22e156da4d6fc6e1a30784de6ac6f867d3
Sangeet1997 <sangeetsaha1116@gmail.com>
2025-02-25 22:59:58 -0500

    removed wouter hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { Router, Route, Switch } from "wouter";
-import { useHashLocation } from "wouter/hash";
+import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -15,45 +14,37 @@ import HobbyDetail from "@/pages/HobbyDetail";
 import Timeline from "@/pages/Timeline";
 import NotFound from "@/pages/not-found";
 import { useEffect } from "react";
-
-function RouterComponent() {
-  // Use the hash location hook
-  const [location] = useHashLocation();
-  
+function Router() {
+  const location = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
-  
   return (
     <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/projects" component={Projects} />
-      <Route path="/projects/:id" component={ProjectDetail} />
-      <Route path="/timeline" component={Timeline} />
-      <Route path="/experience" component={Experience} />
-      <Route path="/experience/:id" component={ExperienceDetail} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/hobbies" component={Hobbies} />
-      <Route path="/hobbies/:id" component={HobbyDetail} />
+      <Route path="/personal_portfolio" component={Home} />
+      <Route path="/personal_portfolio/projects" component={Projects} />
+      <Route path="/personal_portfolio/projects/:id" component={ProjectDetail} />
+      <Route path="/personal_portfolio/timeline" component={Timeline} />
+      <Route path="/personal_portfolio/experience" component={Experience} />
+      <Route path="/personal_portfolio/experience/:id" component={ExperienceDetail} />
+      <Route path="/personal_portfolio/contact" component={Contact} />
+      <Route path="/personal_portfolio/hobbies" component={Hobbies} />
+      <Route path="/personal_portfolio/hobbies/:id" component={HobbyDetail} />
       <Route component={NotFound} />
     </Switch>
   );
 }
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router hook={useHashLocation}>
-        <div className="min-h-screen bg-background">
-          <Navigation />
-          <main className="container mx-auto px-4 pt-16">
-            <RouterComponent />
-          </main>
-        </div>
-      </Router>
+      <div className="min-h-screen bg-background">
+        <Navigation />
+        <main className="container mx-auto px-4 pt-16">
+          <Router />
+        </main>
+      </div>
       <Toaster />
     </QueryClientProvider>
   );
 }
-
 export default App;
